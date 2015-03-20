7393f08ab2d2275ca157d1178fac0148f60b0068
Harlan Stewart 2 <Hstewart12588@gmail.com>
2015-03-20 01:58:26 -0700

    First commit from desktop pc

--- app/src/main/java/com/hmsoly/criminalintent/Crime.java
+++ app/src/main/java/com/hmsoly/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.hmsoly.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,10 +9,29 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private Boolean mSolved;
 
     public Crime(){
         //Generates unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public Boolean getSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(Boolean mSolved) {
+        this.mSolved = mSolved;
     }
 
     public UUID getId() {
